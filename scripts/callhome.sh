82e6a5d1e729e7967dbfbc8c39c1734d8ce4dfff
Clark A <clarka@gmail.com>
2013-08-14 01:15:07 -0700

    add utility to call home

---
+++ scripts/callhome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+home=$(cat /srv/home)
+hostn=$(hostname)
+ipaddr=$(hostname -I| sed 's/ *$//g'|tr ' ' ',')
+
+url=https://$(echo $home/track?say=hello\&host="$hostn"\&ip="$ipaddr"|tr '\n' ' '|tr -d ' ')
+curl --connect-timeout 10 --insecure $url
