b2f435a42a31657571b6d16b05878448aabd3617
Clark A <clarka@gmail.com>
2013-08-15 01:25:21 -0700

    let callhome take an argument

--- scripts/callhome.sh
+++ scripts/callhome.sh
@@ -4,5 +4,5 @@ home=$(cat /srv/home)
 hostn=$(hostname)
 ipaddr=$(hostname -I| sed 's/ *$//g'|tr ' ' ',')
 
-url=https://$(echo $home/track?say=hello\&host="$hostn"\&ip="$ipaddr"|tr '\n' ' '|tr -d ' ')
+url=https://$(echo $home/track?say=hello\&host="$hostn"\&ip="$ipaddr"|tr '\n' ' '|tr -d ' ')\&args=$1
 curl --connect-timeout 10 --insecure $url
