21f245f240d6305f3767979af573d52eba4483e2
Clark A <clarka@gmail.com>
2013-08-15 01:26:17 -0700

    make relative

--- scripts/atboot.sh
+++ scripts/atboot.sh
@@ -1,11 +1,17 @@
-!#/bin/sh
+#!/bin/sh
+
+SCRIPT_DIR=$(readlink -f ${0%/*})
 
 ## make sure the scripts are executable
-chmod -R 740 *.js *.sh
-dos2unix *.js *.sh && dos2unix scripts/*.js scripts/*.sh
+chmod -R 740 $SCRIPT_DIR/*.js $SCRIPT_DIR/*.sh
+chmod -R 740 $SCRIPT_DIR/../*.js $SCRIPT_DIR/../*.sh
+dos2unix $SCRIPT_DIR/../*.js $SCRIPT_DIR/../*.sh
+dos2unix $SCRIPT_DIR/*.js $SCRIPT_DIR/*.sh
 
 ## run firstboot check
-./firstboot.sh
+$SCRIPT_DIR/firstboot.sh
 
 ## set the initial heartbeat file (used by app.js and watchdog.js)
 echo 0 > /tmp/heartbeat
+
+$SCRIPT_DIR/callhome.sh booted
--- scripts/firstboot.sh
+++ scripts/firstboot.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(readlink -f ${0%/*})
+
 #todo set hostname
 #todo call expandFileSystem.sh
-
-echo '#!/bin/bash' > firstboot.sh
+$SCRIPT_DIR/callhome.sh firstboot
+#leave firstboot empty
+mv $SCRIPT_DIR/firstboot.sh $SCRIPT_DIR/firstboot.sh.done
+echo '#!/bin/bash' > $SCRIPT_DIR/firstboot.sh
