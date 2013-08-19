2c6beeffbcc8bc053cadc0a822d140e052078b72
Clark A <clarka@gmail.com>
2013-08-18 18:07:20 -0700

    log booted time and start heartbeat led

--- scripts/atboot.sh
+++ scripts/atboot.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR=$(readlink -f ${0%/*})
 
+#set heartbeat led
+$SCRIPT_DIR/ledheartbeat.sh 1
+
 ## make sure the scripts are executable
 chmod -R 740 $SCRIPT_DIR/*.js $SCRIPT_DIR/*.sh
 chmod -R 740 $SCRIPT_DIR/../*.js $SCRIPT_DIR/../*.sh
@@ -15,3 +18,5 @@ $SCRIPT_DIR/firstboot.sh
 echo 0 > /tmp/heartbeat
 
 $SCRIPT_DIR/callhome.sh booted
+
+echo $(date) >> /srv/booted
